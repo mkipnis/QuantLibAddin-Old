cc67dfcb5877a5d727d8487935e8faec867777f5
Mike Kipnis <mike.kipnis@gmail.com>
2025-05-10 21:17:42 -0400

    cmake and quantlib version update

---
+++ build_with_cmake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+set -x
+
+# Define directories
+QL_REST_SOURCE_DIR="$(pwd)"
+INSTALL_DIR="${1:-$HOME/}"
+LD_LIBRARY_PATH="LD_LIBRARY_PATH"
+
+mkdir -p "$INSTALL_DIR"
+mkdir -p build
+cd build
+
+cmake .. -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR"
+
+cmake --build . --target install --config Debug
