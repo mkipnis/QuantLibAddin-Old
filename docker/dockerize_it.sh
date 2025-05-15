a1c633a74350d152eef0025c6c940120a6d7acc9
Mike Kipnis <mike.kipnis@gmail.com>
2025-05-15 02:19:26 +0000

    updated docker build

--- docker/dockerize_it.sh
+++ docker/dockerize_it.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --no-cache -t ghcr.io/mkipnis/quantlib-addin-old:latest -f docker_build_all .
-#docker push ghcr.io/mkipnis/quantlib-addin-old:latest 
+docker build -t ghcr.io/mkipnis/quantlib-addin-old:latest -f docker_build_all .
+docker push ghcr.io/mkipnis/quantlib-addin-old:latest 
